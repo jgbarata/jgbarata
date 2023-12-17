cd40ba3d9563e85ba36da134a5e1210b7d3e272f
jgbarata <98953536+jgbarata@users.noreply.github.com>
2023-12-17 11:28:02 +0000

    patch_20231217_1127

---
+++ js/galleryopen.js
@@ -0,0 +1,19 @@
+const OverflowToggles = document.querySelectorAll('.column-card');
+
+const enableOverflow = () => {
+  document.body.classList.add('overflow');
+}
+
+const disableOverflow = () => {
+  document.body.classList.remove('overflow');
+}
+
+OverflowToggles.forEach((OverflowToggle) => {
+  OverflowToggle.addEventListener('click', () => {
+    if (!document.body.classList.contains('overflow')) {
+      enableOverflow();
+    } else {  
+      disableOverflow(); 
+    }
+  });
+});
